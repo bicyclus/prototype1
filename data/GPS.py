c804ecf0c91330f8e673542dc8360c6fa92f53b6
r0462183 <lennart.bulteel@student.kuleuven.be>
2014-10-20 17:28:41 +0000

    Optimized

--- data/GPS.py
+++ data/GPS.py
@@ -1,14 +1,9 @@
 import serial
-arduino = serial.Serial('/dev/serial/by-id/usb-Gravitech_ARDUINO_NANO_13BP1184-if00-port0', 9600)
+arduino = serial.Serial('/dev/serial/by-id/usb-Gravitech_ARDUINO_NANO_13BP1066-if00-port0', 9600)
 
-start = False
-end = False
-
-while start == False:
-    if arduino.readline() == "Start":   
-        start = True
-
-while end == False:
-    if arduino.readline() == "End":
-        end = True
-    Location = arduino.readline()
+while True:   
+    if arduino.readline().strip() == '1337':
+        ln = eval(arduino.readline().strip())
+        lt = eval(arduino.readline().strip())
+        print ln,lt
+        
