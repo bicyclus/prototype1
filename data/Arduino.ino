edafc1f7ca3d0f67818090864f0e8eaa8ddbf1cd
PieterjanB <pieter_jan@live.be>
2014-10-20 17:31:21 +0200

    Arduino.ino file added with push button code

---
+++ data/Arduino.ino
@@ -0,0 +1,17 @@
+int button=2;
+int sensorValue;
+void setup(){
+  pinMode(button, INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  sensorValue = digitalRead(button);
+  if (sensorValue == LOW) {
+    Serial.println("Hallo");
+  }  
+  delay(250);
+
+}
+
+
