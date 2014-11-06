d61bf8781c41383715b9fe5dbf1ae3b88d8d9b27
MichielDe Deken <michiel.dedeken@student.kuleuven.be>
2014-11-06 16:26:37 +0100

    Temperature plot working

--- visualisatie/js/trips.js
+++ visualisatie/js/trips.js
@@ -153,24 +153,24 @@ function getTemperature(data){
                 var temp = data[i].sensorData[a];
                 if ((temp.sensorID=="3")&& !(temp.data === undefined)){
                     var timestampDate = new Date(temp.timestamp);
-                    console.log(timestampDate,temp.data[0]);
-                    tempArray.push([timestampDate,temp.data[0].temperature]);
-                    if (tempArray.length>0){
-                        tempArray.sort(SortByTimestamp);
-                        options={'title':'Temperature: '+data[i]._id,colors:['red'],curveType:'function',backgroundColor:'#f5f5f5'};
-                        var chartData = new google.visualization.DataTable();
-                        chartData.addColumn('string', 'Time');
-                        chartData.addColumn('number', 'Temperature');
-                        for (var b = 0; b < tempArray.length; b++){
-                            chartData.addRow(['',tempArray[b][1]]);
-                        }
-                        var draw_temp_div = $('<div></div>');
-                        $("#temp_div").append(draw_temp_div);
-                        var chart = new google.visualization.LineChart(draw_temp_div[0]); //Chart aanmaken in div
-                        chart.draw(chartData, options); //Tekenen
-
-                   }
+                    console.log(temp.data[0]);
+                    tempArray.push([timestampDate,temp.data[0].value]);
                 }
+            }
+            if (tempArray.length>0){
+                tempArray.sort(SortByTimestamp);
+                options={'title':'Temperature: '+data[i]._id,colors:['red'],curveType:'function',backgroundColor:'#f5f5f5'};
+                var chartData = new google.visualization.DataTable();
+                chartData.addColumn('string', 'Time');
+                chartData.addColumn('number', 'Temperature');
+                for (var b = 0; b < tempArray.length; b++){
+                    chartData.addRow(['',tempArray[b][1]]);
+                }
+                var draw_temp_div = $('<div></div>');
+                $("#temp_div").append(draw_temp_div);
+                var chart = new google.visualization.LineChart(draw_temp_div[0]); //Chart aanmaken in div
+                chart.draw(chartData, options); //Tekenen
+
             }
         }
     }
@@ -348,36 +348,3 @@ Array.prototype.equals = function (array) { //Compare full arrays
     return true;
 }
 
-
-function getTemperature(data) {
-    var dataArrayTemp;
-    var options;
-    for (i = 0; i < data.length; i++) { //Iterate over all trips
-        if (!(data[i].sensorData === undefined)) {
-            dataArrayTemp = [];
-            for (a = 0; a < data[i].sensorData.length; a++) { //Iterate over all sensorData
-                var tempData = data[i].sensorData[a];
-                if ((tempData.sensorID == "3") && !(tempData.data === undefined)) {
-                    if (!(tempData.data.value === undefined)) {
-                        var timestampDate = new Date(tempData.timestamp);
-                        dataArrayTemp.push([timestampDate, tempData.data[0].temperature]);
-                        if (dataArrayTemp.length > 0) {
-                            dataArrayTemp.sort(SortByTimestamp);
-                            options = {'title': 'Temperatuur: ' + data[i]._id, colors: ['red'], curveType: 'function', backgroundColor: '#f5f5f5'};
-                            var chartData = new google.visualization.DataTable();
-                            chartData.addColumn('string', 'Time');
-                            chartData.addColumn('number', 'Temperatuur');
-                            for (var b = 0; b < dataArrayTemp.length; b++) {
-                                chartData.addRow(['', dataArray[b][1]]);
-                            }
-                            var draw_div_temp = $('<div></div>');
-                            $("#temp_div").append(draw_div_temp);
-                            var chart = new google.visualization.LineChart(draw_div_temp[0]); //Chart aanmaken in div
-                            chart.draw(chartData, options); //Tekenen
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
