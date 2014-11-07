0b1b8b69aaf1143671f818c5005d50f33c7e54f1
EvertEt <evert.etienne@telenet.be>
2014-11-07 20:11:57 +0100

    Fixed humidity bug

--- visualisatie/js/trips.js
+++ visualisatie/js/trips.js
@@ -16,7 +16,6 @@ function tripInit(){
     initDatepickers();
     $('#myReciever').hide();
     $('#tripProgress').hide();
-
 }
 
 function getAllTrips(){ //JSON van alle trips opvragen en naar functies doorgeven
@@ -75,12 +74,18 @@ function getAllTrips(){ //JSON van alle trips opvragen en naar functies doorgeve
             $('#myReciever').append(JSON.stringify(response));
             progressTrips = progressTrips + 100/PROG_STEPS_TRIPS;
             $('#tripProgressBar').animate({ width: progressTrips.toString()+'%' },ANIM_TIME);
+            console.log('1');
             checkProgressTrips();
             drawChart(response);
+            console.log('2');
             drawAccel(response);
+            console.log('3');
             createMap(response);
+            console.log('4');
             getHumidity(response);
+            console.log('5');
             getTemperature(response);
+            console.log('6');
         }
     });
 }
@@ -115,7 +120,7 @@ function drawAccel(data){
             dataArray  = [];
             for (a = 0; a < data[i].sensorData.length; a++) { //Iterate over all sensorData
                 var accelData = data[i].sensorData[a];
-                if ((accelData.sensorID == "5") && !(accelData.data === undefined)) {
+                if ((accelData.sensorID == "5") && !(accelData.data === undefined) && !(accelData.data[0] === undefined)) {
                     try {
                         if (!(accelData.data[0].acceleration === undefined)) {
                             var timestampDate = new Date(accelData.timestamp);
@@ -170,21 +175,21 @@ function getTemperature(data) {
                     }
 
                 }
-                if (tempArray.length > 0) {
-                    tempArray.sort(SortByTimestamp);
-                    options = {'title': 'Temperature: ' + data[i]._id, colors: ['red'], curveType: 'function', backgroundColor: '#f5f5f5'};
-                    var chartData = new google.visualization.DataTable();
-                    chartData.addColumn('string', 'Time');
-                    chartData.addColumn('number', 'Temperature');
-                    for (var b = 0; b < tempArray.length; b++) {
-                        chartData.addRow(['', tempArray[b][1]]);
-                    }
-                    var draw_temp_div = $('<div></div>');
-                    $("#temp_div").append(draw_temp_div);
-                    var chart = new google.visualization.LineChart(draw_temp_div[0]); //Chart aanmaken in div
-                    chart.draw(chartData, options); //Tekenen
-
+            }
+            if (tempArray.length > 0) {
+                tempArray.sort(SortByTimestamp);
+                options = {'title': 'Temperature: ' + data[i]._id, colors: ['red'], curveType: 'function', backgroundColor: '#f5f5f5'};
+                var chartData = new google.visualization.DataTable();
+                chartData.addColumn('string', 'Time');
+                chartData.addColumn('number', 'Temperature');
+                for (var b = 0; b < tempArray.length; b++) {
+                    chartData.addRow(['', tempArray[b][1]]);
                 }
+                var draw_temp_div = $('<div></div>');
+                $("#temp_div").append(draw_temp_div);
+                var chart = new google.visualization.LineChart(draw_temp_div[0]); //Chart aanmaken in div
+                chart.draw(chartData, options); //Tekenen
+
             }
         }
     }
