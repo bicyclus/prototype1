83c090c5259ec59fbb56319183406900333c68bc
EvertEt <evert.etienne@telenet.be>
2014-11-10 17:54:14 +0100

    calendar accel plots

--- visualisatie/js/const.js
+++ visualisatie/js/const.js
@@ -10,10 +10,12 @@ var PROG_STEPS_TRIPS = 7;
 var PROG_STEPS_CAL = 2;
 //- Initfcts
 //- Fill Calendar
-var PROG_STEPS_SINGLETRIP = 3;
+var PROG_STEPS_SINGLETRIP = 5;
 //- Find trip
-//- GMap
+//- GMap + Iterate
 //- GElev
+//- Accel acc
+//- Accel pos
 var BEGIN_PERCENT = 1.618;
 var ANIM_TIME = 200;
 var ELEV_SAMPLE = 128;//2-512
